f34efa4bcb6d8e15209c53b28ab6354a5e01cfcf
Kondász Ferenc <kondaszferenc@Kondaszs-iMac.local>
2017-12-04 00:26:02 +0100

    added new compoent

---
+++ src/app/modal/deal-detail/deal-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
+import { Deal } from '../../model/Deal';
+
+
+@Component({
+  selector: 'deal-detail',
+  templateUrl: './deal-detail.component.html',
+  styleUrls: ['./deal-detail.component.css']
+})
+export class DealDetailComponent extends DialogComponent<Deal, boolean>
+                                 implements OnInit {
+
+  constructor(dialogService: DialogService) {
+      super(dialogService);
+  }
+
+  ngOnInit() {}
+
+}
